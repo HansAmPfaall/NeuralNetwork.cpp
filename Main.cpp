40490744e564ab7f724b653638af2fe40bb1acb1
HansAmPfaall <EdgarJustus94@gmail.com>
2019-02-24 18:01:21 +0100

    setting up for nn

--- Main.cpp
+++ Main.cpp
@@ -3,22 +3,34 @@
 #include "./NeuralNetwork.cpp"
 
 int main(int argc, char const *argv[]) {
-  NeuralNetwork nn(2, 4, 1);
-  std::vector<float> v;
-  v.push_back(1);
-  v.push_back(3);
-  v.push_back(3);
-  v.push_back(7);
-  nn.calc(v);
-  // for (size_t j = 0; j < 10; j++) {
-  //   auto start = std::chrono::system_clock::now();
-  //   for (size_t i = 0; i < 10000; i++) {
-  //     Matrix matrix(30, 80, 0);
-  //     matrix.mult(matrix.t());
-  //   }
-  //   auto end = std::chrono::system_clock::now();
-  //   auto elapsed = end - start;
-  //   std::cout << elapsed.count() / 1000000000.0 << '\n';
-  // }
+  srand(time(NULL));
+  // srand(0);
+  NeuralNetwork nn(2, 8, 1);
+
+  std::vector<std::vector<float> > v;
+
+  v.push_back({1, 0});
+  v.push_back({1, 1});
+  v.push_back({0, 0});
+  v.push_back({0, 1});
+
+  std::vector<std::vector<float> > t;
+
+  t.push_back({1});
+  t.push_back({0});
+  t.push_back({0});
+  t.push_back({1});
+
+  for (size_t i = 0; i < 400000; i++) {
+
+    int randomIndex = rand() % v.size();
+    nn.train(v[randomIndex], t[randomIndex]);
+  }
+
+  std::cout << nn.calc({0, 0})[0] << std::endl;
+  std::cout << nn.calc({1, 1})[0] << std::endl;
+  std::cout << nn.calc({1, 0})[0] << std::endl;
+  std::cout << nn.calc({0, 1})[0] << std::endl;
+
   return 0;
 }
