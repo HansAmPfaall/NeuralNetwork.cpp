0575d6db940e9fb9b7555f4ebcc4ebbcf4e5e9d9
HansAmPfaall <EdgarJustus94@gmail.com>
2019-05-14 15:29:27 +0200

    tested more input layers

--- Main.cpp
+++ Main.cpp
@@ -5,21 +5,46 @@
 int main(int argc, char const *argv[]) {
   srand(time(NULL));
   // srand(0);
-  NeuralNetwork nn(2, 32, 1, 0.4);
+  NeuralNetwork nn(4, 16, 2, 0.2);
 
   std::vector<std::vector<float> > v;
 
-  v.push_back({0, 0});
-  v.push_back({0, 1});
-  v.push_back({1, 0});
-  v.push_back({1, 1});
+  v.push_back({0, 0, 0, 0});
+  v.push_back({0, 0, 0, 1});
+  v.push_back({0, 0, 1, 0});
+  v.push_back({0, 0, 1, 1});
+  v.push_back({0, 1, 0, 0});
+  v.push_back({0, 1, 0, 1});
+  v.push_back({0, 1, 1, 0});
+  v.push_back({0, 1, 1, 1});
+  v.push_back({1 ,0, 0, 0});
+  v.push_back({1 ,0, 0, 1});
+  v.push_back({1 ,0, 1, 0});
+  v.push_back({1 ,0, 1, 1});
+  v.push_back({1 ,1, 0, 0});
+  v.push_back({1 ,1, 0, 1});
+  v.push_back({1 ,1, 1, 0});
+  v.push_back({1 ,1, 1, 1});
 
   std::vector<std::vector<float> > t;
 
-  t.push_back({0});
-  t.push_back({1});
-  t.push_back({1});
-  t.push_back({0});
+  t.push_back({0, 1});
+  t.push_back({1, 0});
+  t.push_back({1, 0});
+  t.push_back({0, 1});
+  t.push_back({0, 1});
+  t.push_back({1, 0});
+  t.push_back({1, 0});
+  t.push_back({0, 1});
+  t.push_back({0, 1});
+  t.push_back({1, 0});
+  t.push_back({1, 0});
+  t.push_back({0, 1});
+  t.push_back({0, 1});
+  t.push_back({1, 0});
+  t.push_back({1, 0});
+  t.push_back({0, 1});
+
 
   for (size_t i = 0; i < 10000000; i++) {
     int ri = rand() % v.size();
@@ -27,10 +52,11 @@ int main(int argc, char const *argv[]) {
     // std::cout << v[ri][0] << v[ri][1] << t[ri][0] << std::endl;
   }
 
-  std::cout << std::round(nn.calc({0, 0})[0]) << std::endl;
-  std::cout << std::round(nn.calc({1, 0})[0]) << std::endl;
-  std::cout << std::round(nn.calc({0, 1})[0]) << std::endl;
-  std::cout << std::round(nn.calc({1, 1})[0]) << std::endl;
+  std::vector<float> output;
+  for (size_t i = 0; i < v.size(); i++) {
+    output = nn.calc(v[i]);
+    std::cout << output[0] << " != " << t[i][0] << std::endl;
+  }
 
   return 0;
 }
